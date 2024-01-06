e29402f0ebcad45028f4a3c87179dbfc728fcc20
pingfanfan <pingfan.work@gmail.com>
2024-01-06 15:01:18 +0000

    4nd

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function displayChar() {
     var char = document.getElementById("inputChar").value;
-    if (char.length === 1) {
+    if (char.length > 1) {
         document.getElementById("displayArea").innerText = char;
     } else {
-        alert("请输入一个字！");
+        alert("请输入字！");
     }
 }
 
